6babb823d4e13dc1eab73084acad326e64a3ec08
Tudor <tgoncear@gmail.com>
2021-11-02 16:40:02 +0100

    WIP

---
+++ tests/Feature/videos/VideoTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature\videos;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class VideoTest extends TestCase
+{
+    /**
+     * test
+     */
+    public function user_can_view_videos()
+    {
+        $video = Video::create([
+            'title' => 'Ubuntu 101',
+            'description' => '# Here description',
+            'url' => 'https://youtu.be/w8j07_DBl_I',
+            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
+            'previous' => null,
+            'next' => null,
+            'series_id' => 1
+        ]);
+        $response = $this->get('/videos/' . $video->id);
+        //Comprovacions
+        $response->assertStatus(200);
+        $response->assertSee('Ubuntu 101');
+        $response->assertSee('Here description');
+    }
+}
