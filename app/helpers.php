8da38695c19154487227a0eb815a0812fbb24292
Tudor <tgoncear@gmail.com>
2021-11-15 17:29:15 +0100

    [200~Testos unitaris solució exercicis proposats, helpers i refactoritzacions~

--- app/helpers.php
+++ app/helpers.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Team;
 use App\Models\User;
 use App\Models\Video;
 use Carbon\Carbon;
@@ -8,18 +9,27 @@ use Illuminate\Support\Facades\Hash;
 if(!function_exists('create_default_user')){
     function create_default_user(){
 
-        User::create([
+        $userProf = User::create([
             'name' => (config('casteaching.default_user.nameSergi')),
             'email'=> (config('casteaching.default_user.emailSergi')),
             'password'=>Hash::make(config('casteaching.default_user.passwordSergi'))
         ]);
 
-        User::create([
+        $userAlumn = User::create([
             'name' => (config('casteaching.default_user.nameTudor')),
             'email'=> (config('casteaching.default_user.emailTudor')),
             'password'=>Hash::make(config('casteaching.default_user.passwordTudor'))
         ]);
-        Team::create
+        Team::create([
+            'name' => $userProf ->name . "'s Team",
+            'user_id' => $userProf -> id,
+            'personal_team' => true,
+        ]);
+        Team::create([
+            'name' => $userAlumn ->name . "'s Team",
+            'user_id' => $userAlumn -> id,
+            'personal_team' => true,
+        ]);
     }
 }
 if(!function_exists('create_default_video')){
