3760a3f67a205700fd1275c97058cc03bd805b94
Tudor <tgoncear@gmail.com>
2021-11-15 19:26:26 +0100

    La mitad del video 8

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,5 +1,7 @@
+<x-casteaching-layout>
 <h1>{{$video->title}}}</h1>
 <ul>
     <li>descripció : {{$video->description}}</li>
     <li>data : {{$video->published_at}}</li>
 </ul>
+</x-casteaching-layout>
