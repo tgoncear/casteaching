a9c9f01c646ebf9fffc843d200590aa609ccbc2b
Tudor <tgoncear@gmail.com>
2021-11-16 17:31:45 +0100

    Video 109 2021 11 10 Aplicar Estils des de zero Criteris de disseny TailwindCSS i Laravel mix fet

--- resources/views/videos/show.blade.php
+++ resources/views/videos/show.blade.php
@@ -1,7 +1,19 @@
 <x-casteachin-layout>
-<h1>{{$video->title}}}</h1>
-<ul>
-    <li>descripció : {{$video->description}}</li>
-    <li>data : {{$video->published_at}}</li>
-</ul>
+    <div class="flex flex-col h-screen">
+    <iframe class="md:p-3 lg:p-5 xl:px-10 xl:py-5 2xl:px-20 2xl:py-10 h-4/5"
+            src="https://www.youtube.com/embed/btGr3mPK1dU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen></iframe>
+
+        <div class="md:p-3 lg:p-5 xl:px-10 xl:py-3 2xl:px-20 2xl:py-5">
+            <h2>{{$video->title}}</h2>
+        </div>
+        <div class="md:p-3 lg:p-5 xl:px-10 xl:py-3 2xl:px-20 2xl:py-5">
+            {{$video->description}}
+        </div>
+
+    </div>
+
 </x-casteachin-layout>
