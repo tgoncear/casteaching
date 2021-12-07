4d823c282de4090235cff25935b073f868887ac3
Tudor <tgoncear@gmail.com>
2021-12-07 09:54:41 +0100

    He canviat els usuaris per defecte al ser creats

--- config/casteaching.php
+++ config/casteaching.php
@@ -2,11 +2,11 @@
 return [
     'default_user'=>[
         'passwordSergi' => env('SERGI_USER_PASSWORD','12345678'),
-        'nameSergi' => env('SERGI_USER_NAME','noreply'),
-        'emailSergi' => env('SERGI_USER_EMAIL','noreply@gmail.com'),
+        'nameSergi' => env('SERGI_USER_NAME','Sergi Tur'),
+        'emailSergi' => env('SERGI_USER_EMAIL','sergitur@iesebre.com'),
         'passwordTudor' => env('TUDOR_USER_PASSWORD','12345678'),
-        'nameTudor' => env('TUDOR_USER_NAME','noreply'),
-        'emailTudor' => env('TUDOR_USER_EMAIL','noreply@gmail.com')
+        'nameTudor' => env('TUDOR_USER_NAME','Tudor Goncear'),
+        'emailTudor' => env('TUDOR_USER_EMAIL','tgoncear@iesebre.com')
     ]
 
 ];
