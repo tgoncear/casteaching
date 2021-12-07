a84439af7e97a148f9f116782fcfe40dcdd9ac97
Tudor <tgoncear@gmail.com>
2021-12-07 15:25:05 +0100

    Video 120 acabat

--- resources/views/videos/manage/index.blade.php
+++ resources/views/videos/manage/index.blade.php
@@ -1,37 +1,14 @@
 <x-casteachin-layout>
-    <div class="flex flex-col mt-10">
-        @if(session()->has('status'))
-            <div class="rounded-md bg-green-50 p-4">
-                <div class="flex">
-                    <div class="flex-shrink-0">
-                        <!-- Heroicon name: solid/check-circle -->
-                        <svg class="h-5 w-5 text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
-                    <div class="ml-3">
-                        <p class="text-sm font-medium text-green-800">
-                            {{ session('status') }}
-                        </p>
-                    </div>
-                    <div class="ml-auto pl-3">
-                        <div class="-mx-1.5 -my-1.5">
-                            <button type="button" class="inline-flex bg-green-50 rounded-md p-1.5 text-green-500 hover:bg-green-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-green-50 focus:ring-green-600">
-                                <span class="sr-only">Dismiss</span>
-                                <!-- Heroicon name: solid/x -->
-                                <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        @endif
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{__('Videos')}}
+        </h2>
+    </x-slot>
+    <div class="flex flex-col mt-10 mx-auto sm:px-6 lg:px-8 w-full max-w-7xl">
+        <x-status></x-status>
         @can('videos_manage_create')
-            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+            <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 w-screen">
+                <div class="py-2 align-middle sm:px-6 lg:px-8 w-screen">
                     <div class="p-4">
                         <div class="md:grid md:grid-cols-3 md:gap-6">
                             <div class="md:col-span-1">
@@ -96,19 +73,18 @@
                         </div>
                     </div>
                 </div>
-                </div>
             </div>
 
         @endcan
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-        <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 w-screen">
+        <div class="py-2 align-middle sm:px-6 lg:px-8 w-screen">
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                 <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
                         Videos
                     </h3>
                 </div>
-                <table class="min-w-full divide-y divide-gray-200">
+                <table class="min-w-full w-full divide-y divide-gray-200">
                     <thead class="bg-gray-50">
                     <tr>
                         <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
