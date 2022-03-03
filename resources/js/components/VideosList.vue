d59fda677976bdf9032b9e06e613cf6408dbd6f9
Tudor <tgoncear@gmail.com>
2022-03-03 17:55:07 +0100

    wip

--- resources/js/components/VideosList.vue
+++ resources/js/components/VideosList.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8 w-screen">
-        <div class="py-2 align-middle sm:px-6 lg:px-8 w-screen">
+    <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="py-2 align-middle sm:px-6 lg:px-8">
             <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
                 <div class="bg-white px-4 py-5 border-b border-gray-200 sm:px-6">
                     <h3 class="text-lg leading-6 font-medium text-gray-900">
